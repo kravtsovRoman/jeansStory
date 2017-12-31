6abb36251962d41ecd9b851a2f8d0fb52f6647e8
Kravtsov Roman <rozario91208@gmail.com>
2017-12-31 15:29:31 +0200

    nothing

--- js/common.js
+++ js/common.js
@@ -1,19 +1,3 @@
-// $(document).ready(function(){ 
-// 	$('.menu-left-mobile').hide();
-// });
-
-// $(function(){
-// 	$('.burger').click(function(){
-// 		$('.menu-left-mobile').toggle();
-// 	})
-// })
-
-
-
-		// $('.burger').on('click', function(){
-		// 	$('.menu-left-mobile').addClass('open');
-		// });
-
 $(document).ready(function(){
 	$('.fa-bars').on('click', function(event){
 			$('.menu-left-mobile').slideToggle();
