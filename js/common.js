05aaf93178070950ca0be21f2cf530a77fa69ce3
Kravtsov Roman <rozario91208@gmail.com>
2018-01-02 19:44:17 +0200

    home full done

--- js/common.js
+++ js/common.js
@@ -6,3 +6,21 @@ $(document).ready(function(){
 			event.stopPropagation();
 		});
 });
+
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 100) {
+        $('.menu-left a').addClass('menu-scrol');
+        $('.phone').addClass('menu-scrol');
+        $('.search').addClass('scrol');
+        $('.cart').addClass('menu-scrol');
+        $('.logo img').addClass('menu-scrol');
+    }
+    else {
+        $('.menu-left a').removeClass('menu-scrol');
+        $('.phone').removeClass('menu-scrol');
+        $('.search').removeClass('scrol');
+        $('.cart').removeClass('menu-scrol');
+        $('.logo img').removeClass('menu-scrol');
+    }
+});
