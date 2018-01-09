f11f90298cc1c1500f39afc9fa86d1a01079d7fe
Kravtsov Roman <rozario91208@gmail.com>
2018-01-09 21:19:54 +0200

    Product-> title & price

--- product.php
+++ product.php
@@ -16,7 +16,7 @@
 		<a href="#">Мужские джинсы</a>
 	</li>
 	<li class="current">
-		<a href="#">0041 TOMMY LIFE (28-34,МОЛОДЕЖКА 8 ЕД, ) БРЮКИ МУЖСКИЕ</a>
+		<a href="#">6102 CARACAJ СИНИЕ (25-30, 6 ЕД.) ДЖИНСЫ ЖЕНСКИЕ УЗКИЕ ТЕРКА ДЕМИСЕЗОННЫЕ СТРЕЙЧ</a>
 	</li>
 </ol>
 <div class="clear"></div>
@@ -28,7 +28,6 @@
 			<div class="slider-nav_item"><img src="img/product/product (3).jpg" alt=""></div>
 			<div class="slider-nav_item"><img src="img/product/product (4).jpg" alt=""></div>
 			<div class="slider-nav_item"><img src="img/product/product (5).jpg" alt=""></div>
-
 		</div>
 		<div class="slider-product">
 			<div class="slider_item"><img src="img/watermark/watermark_to_image.php?src=../product/product (1).jpg" alt=""></div>
@@ -39,9 +38,71 @@
 		</div>
 	</div><!--product-foto-->
 	<div class="product-info">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ab ratione quos ullam ut quia quidem, voluptatum blanditiis, libero perspiciatis. Tempore est provident possimus debitis sunt eos laboriosam iusto culpa quae minima aut at, aperiam, vel, unde nesciunt! Sunt ipsam nisi distinctio voluptas rem deleniti, molestias qui animi dolore perspiciatis cum officia provident tempore minus dolores obcaecati eius totam debitis iure consequatur ullam. Sint vitae temporibus cupiditate consequuntur ea assumenda reprehenderit magnam architecto sunt debitis, ipsa error quas. Modi incidunt maxime nihil eum praesentium sit cupiditate adipisci libero quod. Odit iusto nisi qui, sint quasi? Ullam cumque similique incidunt aspernatur minima temporibus sit autem dolore ratione officiis repellat nesciunt tempore, quibusdam atque, accusantium nulla sed culpa, optio sunt quidem odio amet cupiditate explicabo. Dolorum saepe inventore qui repudiandae quo repellat, necessitatibus consectetur fugiat beatae temporibus ipsam rem ducimus facilis asperiores voluptatum quis. Aperiam quidem, mollitia ut facilis atque ea recusandae, voluptatibus reiciendis sequi similique eligendi beatae ex aliquam distinctio omnis veniam quo commodi? Quam eaque dolorem modi facere deserunt nam nesciunt amet non quos libero necessitatibus, laudantium itaque, voluptatum, repellendus. Laboriosam, deleniti quas. Aliquid omnis hic in. Ab molestias, harum laborum quidem, soluta expedita doloribus, cum vel necessitatibus quo atque.
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem minus aspernatur saepe magni distinctio eum molestiae earum dolore reprehenderit! Enim voluptatibus impedit fuga eos magnam, eius molestiae necessitatibus quisquam quidem tenetur nesciunt nobis perferendis placeat ad animi voluptatem, repellat rerum incidunt fugiat nihil, fugit. Laudantium quis ab provident obcaecati nobis aperiam unde, fugiat minus vitae, inventore architecto culpa vero doloremque laboriosam magnam exercitationem, dolor maiores ad repellat. Deserunt omnis ab magnam inventore adipisci, vel repudiandae architecto atque illo quam at nemo. Quae, sed impedit deleniti saepe quisquam in dolor dolorem ducimus accusamus hic animi quo! Quod necessitatibus, commodi. Ratione incidunt fugiat, consequuntur accusantium quas odit, ipsum autem, quo unde saepe neque, nemo aspernatur molestias! Assumenda laboriosam, esse iure optio earum sapiente deleniti iste nam id! Voluptate, eius, natus? Libero reiciendis praesentium debitis rem repellat commodi incidunt enim corporis aliquid vel, facilis ad delectus nulla architecto dicta optio eius nemo eaque ea deserunt, molestiae reprehenderit adipisci doloremque fugiat. Similique nihil omnis aspernatur, quidem minus repudiandae asperiores officia veritatis dicta voluptatem fugiat sed, unde, accusantium est dolorum. Officia accusantium praesentium error unde aspernatur ad magni veniam, eos quia nihil molestias velit, illum quas nisi distinctio saepe pariatur recusandae nostrum. Neque reprehenderit consequuntur impedit sunt voluptates quibusdam, fugiat non voluptate corporis veritatis cupiditate ad quos et adipisci repellendus nesciunt in enim ipsum facere libero nisi! Quo quae iusto quisquam, modi officiis facere? Quod iste, eius quos exercitationem corrupti magni doloremque reiciendis facere debitis commodi nesciunt quidem eveniet sapiente ullam reprehenderit distinctio sint non cum itaque iusto rerum natus. Dolorum enim voluptates assumenda distinctio similique a tempore dolorem quibusdam, corporis nam ad aut laboriosam rem eius harum magni, maiores autem recusandae esse cumque ipsum suscipit! Cum earum recusandae reiciendis, repellat voluptatem voluptate fugiat corporis. Cumque nam quibusdam illum vel voluptate facilis cupiditate voluptates! Nobis voluptatem quibusdam ducimus natus consectetur dicta eligendi laudantium non, modi hic. Blanditiis, facere obcaecati? Aut odit minus provident similique iusto cupiditate quis itaque aspernatur ut. Veritatis saepe nam, accusamus molestias voluptatum reiciendis a tempore! Assumenda, tempore pariatur, totam sunt accusamus officia incidunt asperiores tenetur excepturi facilis neque! Fuga, beatae quo dolores in quis cumque praesentium aliquam esse. Quidem accusamus nihil exercitationem officiis. Soluta, est, aliquam. Minus nisi omnis nemo numquam dignissimos ex, dolores sunt eaque doloremque dolore ab! Ab reprehenderit fuga quos quia minima praesentium modi quis voluptatum esse porro eveniet adipisci soluta cumque obcaecati aliquid nihil repellat ex, ipsam quisquam, rem molestiae possimus cum quo illo! Consectetur, vitae, quo provident voluptatem ratione hic eos accusamus, nam harum rem, voluptate sunt voluptates temporibus aperiam unde dolores nisi. Nisi, quis tempora enim nulla aliquid quo necessitatibus ipsa magnam et! Cum tenetur, pariatur veniam esse reprehenderit laborum eveniet est dolorem animi, ad! Iste sequi officia, cumque placeat saepe magni fugiat ullam necessitatibus quidem eius doloremque rem molestias! Nesciunt eaque, quisquam! Alias iusto corporis eligendi iure eaque debitis consectetur quisquam, repudiandae? Fugit est temporibus accusamus nulla velit ipsum optio excepturi eveniet error voluptas, consectetur quo soluta, repellendus necessitatibus magni nostrum fugiat minima ut.
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam atque vel, nisi aperiam accusamus eum. Vitae maiores, tempore, error aperiam officiis voluptatibus quidem, laudantium magnam iste distinctio veritatis ipsa. Odit id officia nesciunt velit quidem sit placeat culpa iste error quis delectus, adipisci labore aspernatur temporibus veniam accusantium obcaecati, magni at voluptas. Modi quos ad hic harum tempora. Accusantium aliquam inventore quis consectetur, incidunt mollitia. Nostrum, labore debitis provident, quod eligendi officiis porro repellat, incidunt blanditiis dignissimos quaerat quisquam tempora facere placeat possimus perferendis impedit quidem? Consequatur consequuntur hic recusandae quisquam, aliquam vel vitae dolorum nesciunt laudantium maiores in, illum, delectus officia. Blanditiis quibusdam hic commodi officiis eaque odio ipsum, quia, aperiam earum a, vitae. Praesentium ipsam eos nemo adipisci, amet doloremque hic, accusamus repellat quasi dignissimos veritatis! Soluta excepturi porro optio veritatis eaque ullam, nihil quae dolor praesentium. Odio facilis necessitatibus esse amet sit eius asperiores molestiae, porro voluptates vitae dolorum a aliquam labore ullam. Ducimus obcaecati cupiditate delectus perferendis omnis, repellendus soluta, assumenda dolor saepe ea adipisci dolorem distinctio ullam est perspiciatis, enim! Esse atque cupiditate adipisci sapiente harum vero ex id deleniti similique, dolores repellat laboriosam porro ipsum. Atque soluta modi sapiente praesentium vero, dolorum unde, iste dignissimos at suscipit error, in voluptate odio eum laborum nisi consequatur reprehenderit laudantium. Quisquam debitis laboriosam, doloribus maiores! Fugit iusto quos sunt rerum, saepe libero quae veritatis vero voluptatum sapiente voluptatem fuga magni soluta aliquid at! Architecto nesciunt dolorem assumenda, unde illo, vitae sed ipsam voluptas nam similique incidunt. Saepe facere officiis ullam, voluptate porro nostrum labore fuga voluptas non dolor nulla eaque cupiditate sit veritatis est praesentium consequuntur dolores dolorem. Dignissimos eveniet deserunt veniam quos, libero officiis temporibus voluptates eum consectetur possimus, laborum rerum corrupti modi reiciendis laboriosam voluptatibus esse fugit vel. Veritatis distinctio et dignissimos pariatur, blanditiis quaerat!
+		<div class="wrap">
+			<div class="product-name">
+				<h2> 6102 CARACAJ СИНИЕ (25-30, 6 ЕД.) ДЖИНСЫ ЖЕНСКИЕ УЗКИЕ ТЕРКА ДЕМИСЕЗОННЫЕ СТРЕЙЧ 
+				</h2> 
+				<div class="product-barcode">
+					 #<span> 547601 </span>
+				</div>
+			</div>
+			<div class="product-price">
+				<p>12.00 <span><i class="fa fa-usd" aria-hidden="true"></i></span></p>
+				<div class="isset"> В наличии </div>
+			</div>
+		</div>
+		<hr>
+		<div class="wrap">
+			<div class="product-filters">
+				<h4>Характеристики</h4>
+				<table class="table-filters" cellspacing="7">
+				  <tr>
+				    <td>Сезон</td>
+				    <td>
+				    	<a href="#" class="filter-link">Зима</a>
+					</td>
+				  </tr>
+				  <tr>
+				    <td>Размер</td>
+				    <td>
+				    	<a href="#" class="filter-link">Норма</a>
+					</td>
+				  </tr>
+				  <tr>
+				    <td>Стиль</td>
+				    <td>
+				    	<a href="#" class="filter-link">Класика</a>
+					</td>
+				  </tr>
+				  <tr>
+				    <td>Страна</td>
+				    <td>
+				    	<a href="#" class="filter-link">Китай</a>
+					</td>
+				  </tr>
+				  <tr>
+				    <td>Фирма</td>
+				    <td>
+				    	<a href="#" class="filter-link">Gloria</a>
+					</td>
+				  </tr>
+				</table>
+			</div>
+			<div class="product-data">
+				<div class="price-edit">
+					<p> 
+						<span class="product-price-edit"> 12.00 </span>
+						 x 
+						 	<i class="fa fa-angle-double-left" aria-hidden="true"></i>
+						 <span class="product-quantity"> 6 </span>
+						 	<i class="fa fa-angle-double-right" aria-hidden="true"></i>	 
+						  = 
+						  <span class="total-price"> 72 </span>
+						  <i class="fa fa-usd" aria-hidden="true"></i>
+					</p> 
+				</div>
+			</div>
+		</div>
 	</div>
 </div><!--wrapper-->
 
